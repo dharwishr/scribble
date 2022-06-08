09a99784605a5cdde6d20ab8219246c9d470a2e4
Dharwish Raj <drpmiro@gmail.com>
2022-06-08 20:30:39 +0530

    Added Create route in category API and controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,4 +5,20 @@ class CategoriesController < ApplicationController
     categories = Category.all
     render status: :ok, json: { categories: categories }
   end
+
+  def create
+    category = Category.new(category_params)
+    category.save!
+    respond_with_success("successfully_created")
+  end
+
+  private
+
+    def category_params
+      params.require(:category).permit(:category)
+    end
+
+    def load_category!
+      @category = Category.find_by!(slug: params[:slug])
+    end
 end
