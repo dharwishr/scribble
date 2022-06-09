1c61298b6b44f83b7cecce0fe661ed717415fc2d
Dharwish Raj <drpmiro@gmail.com>
2022-06-09 07:54:43 +0530

    Added category delete, edit, and serach

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -12,13 +12,24 @@ class CategoriesController < ApplicationController
     respond_with_success("successfully_created")
   end
 
+  def update
+    category = Category.find_by!(id: params[:id])
+    category.update!(category_params)
+    respond_with_success("successfully_updated")
+  end
+
+  def destroy
+    @category.destroy!
+    respond_with_json
+  end
+
   private
 
     def category_params
-      params.require(:category).permit(:category)
+      params.require(:category).permit(:id, :category)
     end
 
     def load_category!
-      @category = Category.find_by!(slug: params[:slug])
+      @category = Category.find_by!(id: params[:id])
     end
 end
--- app/javascript/src/apis/categories.js
+++ app/javascript/src/apis/categories.js
@@ -4,9 +4,15 @@ const list = () => axios.get("/categories");
 
 const create = payload => axios.post("/categories/", payload);
 
+const update = (id, payload) => axios.put(id, payload);
+
+const destroy = id => axios.delete(id);
+
 const categoriesApi = {
   list,
   create,
+  update,
+  destroy,
 };
 
 export default categoriesApi;
